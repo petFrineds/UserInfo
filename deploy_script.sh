28329c6bb31497269cdc876d189d81596f1b579e
shicrystal <35886279+shicrystal@users.noreply.github.com>
2022-09-09 00:36:57 +0900

    Create deploy_script.sh

---
+++ deploy_script.sh
@@ -0,0 +1,10 @@
+git pull
+docker build -t userinfo-backend .
+docker tag userinfo-backend:latest 811288377093.dkr.ecr.$AWS_REGION.amazonaws.com/userinfo-backend:latest
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin 811288377093.dkr.ecr.us-west-2.amazonaws.com/
+docker push 811288377093.dkr.ecr.us-west-2.amazonaws.com/userinfo-backend:latest
+kubectl delete -f manifests/userinfo-deployment.yaml
+kubectl apply -f manifests/userinfo-deployment.yaml
+kubectl get pod
+kubectl apply -f manifests/userinfo-service.yaml
+kubectl get service
